f9e9b45806b3774845458ebe859e61512c4a8b8e
Souviksamanta34 <souviksamanta34@.com>
2024-10-04 16:14:39 +0530

    header changed

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,5 @@
-import React from "react";
+import React from "react"; 
 import "./Header.css";
-import SearchIcon from '@mui/icons-material/Search';
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 import { Link } from "react-router-dom";
 import { useStateValue } from "./StateProvider";
@@ -12,7 +11,6 @@ function Header() {
   const handleAuthentication = () => {
     if (user) {
       auth.signOut().then(() => {
-        // After signing out, remove user from global state
         dispatch({
           type: "SET_USER",
           user: null,
@@ -25,39 +23,30 @@ function Header() {
 
   return (
     <div className="header">
+      <div className="header__left">
+        <Link to="/orders">
+          <div className="header__option">
+            <span className="header__optionLineTwo">Orders</span>
+          </div>
+        </Link>
+
+        <Link to="/checkout">
+          <div className="header__optionBasket">
+            <ShoppingBasketIcon sx={{ fontSize: 45 }} /> 
+            <span className="header__optionLineTwo header__basketCount">{basket?.length}</span>
+          </div>
+        </Link>
+      </div>
+
       <Link to="/">
         <img className="header__logo" src="https://i.ibb.co/QYW747Z/logo.png" alt="Logo" />
       </Link>
 
-      <div className="header__search">
-        <input className="header__searchInput" type="text" />
-        <SearchIcon className="header__searchIcon" />
-      </div>
-
-      <div className="header__nav">
+      <div className="header__right">
         <Link to={!user && "/login"}>
           <div onClick={handleAuthentication} className="header__option">
-            <span className="header__optionLineOne">Hello {!user ? "Guest" : user.email}</span>
+            <span className="header__optionLineTwo">Hello {!user ? "," : user.email}</span>
             <span className="header__optionLineTwo">{user ? "Sign Out" : "Sign In"}</span>
-          </div>
-        </Link>
-
-        <Link to="/orders">
-          <div className="header__option">
-            <span className="header__optionLineOne">Returns</span>
-            <span className="header__optionLineTwo">& Orders</span>
-          </div>
-        </Link>
-
-        <div className="header__option">
-          <span className="header__optionLineOne"></span>
-          <span className="header__optionLineTwo"></span>
-        </div>
-
-        <Link to="/checkout">
-          <div className="header__optionBasket">
-            <ShoppingBasketIcon />
-            <span className="header__optionLineTwo header__basketCount">{basket?.length}</span>
           </div>
         </Link>
       </div>
