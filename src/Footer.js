8faf61cad79939edca7c391d0b25d283ef892afa
Souviksamanta34 <souviksamanta34@.com>
2024-10-07 03:05:09 +0530

    redirect file created

--- src/Footer.js
+++ src/Footer.js
@@ -1,33 +1,25 @@
 // Footer.js
 import React from 'react';
-import './Footer.css';  // We will style the footer in a separate CSS file
+import './Footer.css';  
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
     <div className="footer">
       <div className="footer__container">
-        {/* Section 1: About the Company */}
         <div className="footer__section">
-          <h3><a href="/about">About Us</a></h3>
+          <h3><Link to="/about">About Us</Link></h3>
         </div>
-
-        {/* Section 2: Customer Service Links */}
         <div className="footer__section">
-          <h3><a href="/orders">Returns & Orders</a></h3>
+          <h3><Link to="/orders">Returns & Orders</Link></h3>
         </div>
-
         <div className="footer__section">
-          <h3><a href="/contact">Contact Us</a></h3>
+          <h3><Link to="/contact">Contact Us</Link></h3>
         </div>
-
-
-        {/* Section 4: Social Media Icons */}
         <div className="footer__section">
           <h3><a href="https://www.facebook.com" target="_blank" rel="noreferrer">Follow Us</a></h3>
         </div>
       </div>
-
-      {/* Copyright */}
       <div className="footer__copyright">
         <p>&copy; 2024 GruhamStore. All rights reserved.</p>
       </div>
