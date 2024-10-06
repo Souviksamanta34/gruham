2ea5c12676f42ce17fd84271e63525bed5c36732
Souviksamanta34 <souviksamanta34@.com>
2024-10-06 18:20:10 +0530

    footer done

--- src/Footer.js
+++ src/Footer.js
@@ -14,27 +14,16 @@ function Footer() {
         {/* Section 2: Customer Service Links */}
         <div className="footer__section">
           <h3><a href="/orders">Returns & Orders</a></h3>
-          <ul>
-            <li><a href="/contact">Contact Us</a></li>
-          </ul>
         </div>
 
         <div className="footer__section">
-          <h3><a href="/orders">Returns & Orders</a></h3>
-          <ul>
-            <li><a href="/contact">Contact Us</a></li>
-          </ul>
+          <h3><a href="/contact">Contact Us</a></h3>
         </div>
 
 
         {/* Section 4: Social Media Icons */}
         <div className="footer__section">
-          <h3>Follow Us</h3>
-          <div className="footer__social-icons">
-            <a href="https://www.facebook.com" target="_blank" rel="noreferrer">Facebook</a>
-            <a href="https://www.instagram.com" target="_blank" rel="noreferrer">Instagram</a>
-            <a href="https://www.twitter.com" target="_blank" rel="noreferrer">Twitter</a>
-          </div>
+          <h3><a href="https://www.facebook.com" target="_blank" rel="noreferrer">Follow Us</a></h3>
         </div>
       </div>
 
